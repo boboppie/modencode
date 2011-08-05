8354c086fe1660f3be4c1e1c81fd7b00d632029d
Radek Stepan <radek.stepan@gmail.com>
2011-08-05 11:02:55 +0100

    rearranged fake dict

--- flask/models/fakedata.py
+++ flask/models/fakedata.py
@@ -4,61 +4,62 @@
 # TODO: remove this when done
 
 data = {
-    { "organism":
+    "organisms":
         [
-            { "category":
-                [
-                    { "category_title":"Chromatin structure",
-                      "experiment":
-                        [
-                            { "experiment_title":"Genome-wide Chromatin Profiling",
-                              "labs": ["Kamran Ahmad", "Steven Henikoff"],
-                              "pi":"Steven Henikoff"}
-                        ]
-                    },
-                    { "category_title":"Copy Number Variation",
-                      "experiment":
-                        [
-                            { "experiment_title":"CGH drosophila cell lines",
-                              "labs": ["David MacAlpine"],
-                              "pi" : "David MacAlpine"
-                            },
-                            { "experiment_title":"CNV in Drosophila cell lines",
-                              "labs": ["David MacAlpine"],
-                              "pi": "David MacAlpine"
-                            }
-                        ]
-                    }
-                ],
+            {
+                "experiments":
+                    [
+                        { "category_title":"Chromatin structure",
+                          "experiment":
+                            [
+                                { "experiment_title":"Genome-wide Chromatin Profiling",
+                                  "labs": ["Kamran Ahmad", "Steven Henikoff"],
+                                  "pi":"Steven Henikoff"}
+                            ]
+                        },
+                        { "category_title":"Copy Number Variation",
+                          "experiment":
+                            [
+                                { "experiment_title":"CGH drosophila cell lines",
+                                  "labs": ["David MacAlpine"],
+                                  "pi" : "David MacAlpine"
+                                },
+                                { "experiment_title":"CNV in Drosophila cell lines",
+                                  "labs": ["David MacAlpine"],
+                                  "pi": "David MacAlpine"
+                                }
+                            ]
+                        }
+                    ],
                 "org_name":"D. melanogaster"
             },
-            { "category":
-                [
-                    { "category_title":"Chromatin structure",
-                      "experiment":
-                        [
-                            { "experiment_title":"Nucleosome mapping",
-                              "labs": ["Jason Lieb"],
-                              "pi": "Jason Lieb"
-                            }
-                        ]
-                    },
-                    { "category_title":"Gene Structure",
-                      "experiment":
-                        [
-                            { "experiment_title":"Encyclopedia of C. elegans 3'UTRs and their regulatory elements",
-                              "labs": ["Fabio Piano", "John Kim"],
-                              "pi" : "Fabio Piano"
-                            },
-                            { "experiment_title":"SL Acceptor Site Confirmation in C. elegans",
-                              "labs": ["Phil Green"],
-                              "pi": "Robert Waterston"
-                            }
-                        ]
-                    }
-                ],
+            {
+                "experiments":
+                    [
+                        { "category_title":"Chromatin structure",
+                          "experiment":
+                            [
+                                { "experiment_title":"Nucleosome mapping",
+                                  "labs": ["Jason Lieb"],
+                                  "pi": "Jason Lieb"
+                                }
+                            ]
+                        },
+                        { "category_title":"Gene Structure",
+                          "experiment":
+                            [
+                                { "experiment_title":"Encyclopedia of C. elegans 3'UTRs and their regulatory elements",
+                                  "labs": ["Fabio Piano", "John Kim"],
+                                  "pi" : "Fabio Piano"
+                                },
+                                { "experiment_title":"SL Acceptor Site Confirmation in C. elegans",
+                                  "labs": ["Phil Green"],
+                                  "pi": "Robert Waterston"
+                                }
+                            ]
+                        }
+                    ],
                 "org_name":"C. elegans"
             }
         ]
-    }
 }
