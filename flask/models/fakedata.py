8f74248b8e9d8aeb1ca2152e579c31352add06b3
Radek Stepan <rs676@aragorn.(none)>
2011-08-05 16:16:47 +0100

    newer fake data organized around categories

--- flask/models/fakedata.py
+++ flask/models/fakedata.py
@@ -4,62 +4,41 @@
 # TODO: remove this when done
 
 data = {
-    "organisms":
-        [
+    "categories":
+        {   "Chromatin structure":
             {
-                "experiments":
+                "D. melanogaster":
                     [
-                        { "category_title":"Chromatin structure",
-                          "experiment":
-                            [
-                                { "experiment_title":"Genome-wide Chromatin Profiling",
-                                  "labs": ["Kamran Ahmad", "Steven Henikoff"],
-                                  "pi":"Steven Henikoff"}
-                            ]
-                        },
-                        { "category_title":"Copy Number Variation",
-                          "experiment":
-                            [
-                                { "experiment_title":"CGH drosophila cell lines",
-                                  "labs": ["David MacAlpine"],
-                                  "pi" : "David MacAlpine"
-                                },
-                                { "experiment_title":"CNV in Drosophila cell lines",
-                                  "labs": ["David MacAlpine"],
-                                  "pi": "David MacAlpine"
-                                }
-                            ]
+                        {
+                            "experiment_title":"Genome-wide Chromatin Profiling",
+                            "labs": ["Kamran Ahmad", "Steven Henikoff"],
+                            "pi":"Steven Henikoff"
                         }
                     ],
-                "org_name":"D. melanogaster"
+                "C. elegans":
+                    [
+                        {
+                            "experiment_title":"Nucleosome mapping",
+                            "labs": ["Jason Lieb"],
+                            "pi": "Jason Lieb"
+                        }
+                    ]
             },
+            "Copy Number Variation":
             {
-                "experiments":
+                "D. melanogaster":
                     [
-                        { "category_title":"Chromatin structure",
-                          "experiment":
-                            [
-                                { "experiment_title":"Nucleosome mapping",
-                                  "labs": ["Jason Lieb"],
-                                  "pi": "Jason Lieb"
-                                }
-                            ]
+                        {
+                            "experiment_title":"CGH drosophila cell lines",
+                            "labs": ["David MacAlpine"],
+                            "pi" : "David MacAlpine"
                         },
-                        { "category_title":"Gene Structure",
-                          "experiment":
-                            [
-                                { "experiment_title":"Encyclopedia of C. elegans 3'UTRs and their regulatory elements",
-                                  "labs": ["Fabio Piano", "John Kim"],
-                                  "pi" : "Fabio Piano"
-                                },
-                                { "experiment_title":"SL Acceptor Site Confirmation in C. elegans",
-                                  "labs": ["Phil Green"],
-                                  "pi": "Robert Waterston"
-                                }
-                            ]
+                        {
+                            "experiment_title":"CNV in Drosophila cell lines",
+                            "labs": ["David MacAlpine"],
+                            "pi": "David MacAlpine"
                         }
-                    ],
-                "org_name":"C. elegans"
+                    ]
             }
-        ]
+        }
 }
