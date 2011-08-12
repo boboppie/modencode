32013fa538026328f3cd4ed1f0cd91f605aee365
Fengyuan Hu <fengyuan.huu@gmail.com>
2011-08-12 16:40:23 +0100

    modified short author name

--- flask/models/publications.py
+++ flask/models/publications.py
@@ -10,7 +10,7 @@ publications = {
                 {
                     "title": "Unlocking the secrets of the genome.",
                     "full_authors": "Celniker SE, Dillon LA, Gerstein MB, Gunsalus KC, Henikoff S, Karpen GH, Kellis M, Lai EC, Lieb JD, MacAlpine DM, Micklem G, Piano F, Snyder M, Stein L, White KP, Waterston RH; modENCODE Consortium.",
-                    "short_authors": "Celniker SE",
+                    "short_authors": "Celniker SE et al.",
                     "date": "18 June 2009",
                     "vol": "459",
                     "page":"927-930",
@@ -20,7 +20,7 @@ publications = {
                 {
                     "title": "The developmental transcriptome of Drosophila melanogaster.",
                     "full_authors": "Graveley BR, Brooks AN, Carlson JW, Duff MO, Landolin JM, Yang L, Artieri CG, van Baren MJ, Boley N, Booth BW, Brown JB, Cherbas L, Davis CA, Dobin A, Li R, Lin W, Malone JH, Mattiuzzo NR, Miller D, Sturgill D, Tuch BB, Zaleski C, Zhang D, Blanchette M, Dudoit S, Eads B, Green RE, Hammonds A, Jiang L, Kapranov P, Langton L, Perrimon N, Sandler JE, Wan KH, Willingham A, Zhang Y, Zou Y, Andrews J, Bickel PJ, Brenner SE, Brent MR, Cherbas P, Gingeras TR, Hoskins RA, Kaufman TC, Oliver B, Celniker SE.",
-                    "short_authors": "Graveley BR",
+                    "short_authors": "Graveley BR et al.",
                     "date": "24 March 2011",
                     "vol": "471",
                     "page":"473–479",
@@ -30,7 +30,7 @@ publications = {
                 {
                     "title": "Comprehensive analysis of the chromatin landscape in Drosophila melanogaster.",
                     "full_authors": "Kharchenko PV, Alekseyenko AA, Schwartz YB, Minoda A, Riddle NC, Ernst J, Sabo PJ, Larschan E, Gorchakov AA, Gu T, Linder-Basso D, Plachetka A, Shanower G, Tolstorukov MY, Luquette LJ, Xi R, Jung YL, Park RW, Bishop EP, Canfield TK, Sandstrom R, Thurman RE, MacAlpine DM, Stamatoyannopoulos JA, Kellis M, Elgin SC, Kuroda MI, Pirrotta V, Karpen GH, Park PJ.",
-                    "short_authors": "Kharchenko PV",
+                    "short_authors": "Kharchenko PV et al.",
                     "date": "24 March 2011",
                     "vol": "471",
                     "page":"480–485",
@@ -40,7 +40,7 @@ publications = {
                 {
                     "title": "A cis-regulatory map of the Drosophila genome.",
                     "full_authors": "Nègre N, Brown CD, Ma L, Bristow CA, Miller SW, Wagner U, Kheradpour P, Eaton ML, Loriaux P, Sealfon R, Li Z, Ishii H, Spokony RF, Chen J, Hwang L, Cheng C, Auburn RP, Davis MB, Domanus M, Shah PK, Morrison CA, Zieba J, Suchy S, Senderowicz L, Victorsen A, Bild NA, Grundstad AJ, Hanley D, MacAlpine DM, Mannervik M, Venken K, Bellen H, White R, Gerstein M, Russell S, Grossman RL, Ren B, Posakony JW, Kellis M, White KP.",
-                    "short_authors": "Nègre N",
+                    "short_authors": "Nègre N et al.",
                     "date": "24 March 2011",
                     "vol": "471",
                     "page":"527–531",
@@ -57,7 +57,7 @@ publications = {
 
                     "title": "Integrative analysis of the Caenorhabditis elegans genome by the modENCODE project.",
                     "full_authors": "Gerstein MB, Lu ZJ, Van Nostrand EL, Cheng C, Arshinoff BI, Liu T, Yip KY, Robilotto R, Rechtsteiner A, Ikegami K, Alves P, Chateigner A, Perry M, Morris M, Auerbach RK, Feng X, Leng J, Vielle A, Niu W, Rhrissorrakrai K, Agarwal A, Alexander RP, Barber G, Brdlik CM, Brennan J, Brouillet JJ, Carr A, Cheung MS, Clawson H, Contrino S, Dannenberg LO, Dernburg AF, Desai A, Dick L, Dosé AC, Du J, Egelhofer T, Ercan S, Euskirchen G, Ewing B, Feingold EA, Gassmann R, Good PJ, Green P, Gullier F, Gutwein M, Guyer MS, Habegger L, Han T, Henikoff JG, Henz SR, Hinrichs A, Holster H, Hyman T, Iniguez AL, Janette J, Jensen M, Kato M, Kent WJ, Kephart E, Khivansara V, Khurana E, Kim JK, Kolasinska-Zwierz P, Lai EC, Latorre I, Leahey A, Lewis S, Lloyd P, Lochovsky L, Lowdon RF, Lubling Y, Lyne R, MacCoss M, Mackowiak SD, Mangone M, McKay S, Mecenas D, Merrihew G, Miller DM 3rd, Muroyama A, Murray JI, Ooi SL, Pham H, Phippen T, Preston EA, Rajewsky N, Rätsch G, Rosenbaum H, Rozowsky J, Rutherford K, Ruzanov P, Sarov M, Sasidharan R, Sboner A, Scheid P, Segal E, Shin H, Shou C, Slack FJ, Slightam C, Smith R, Spencer WC, Stinson EO, Taing S, Takasaki T, Vafeados D, Voronina K, Wang G, Washington NL, Whittle CM, Wu B, Yan KK, Zeller G, Zha Z, Zhong M, Zhou X; modENCODE Consortium, Ahringer J, Strome S, Gunsalus KC, Micklem G, Liu XS, Reinke V, Kim SK, Hillier LW, Henikoff S, Piano F, Snyder M, Stein L, Lieb JD, Waterston RH.",
-                    "short_authors": "Gerstein MB",
+                    "short_authors": "Gerstein MB et al.",
                     "date": "24 December 2010",
                     "vol": "330 no. 6012",
                     "pp":"1775-1787",
@@ -67,7 +67,7 @@ publications = {
                 {
                     "title": "Identification of Functional Elements and Regulatory Circuits by Drosophila modENCODE.",
                     "full_authors": "modENCODE Consortium, Roy S, Ernst J, Kharchenko PV, Kheradpour P, Negre N, Eaton ML, Landolin JM, Bristow CA, Ma L, Lin MF, Washietl S, Arshinoff BI, Ay F, Meyer PE, Robine N, Washington NL, Di Stefano L, Berezikov E, Brown CD, Candeias R, Carlson JW, Carr A, Jungreis I, Marbach D, Sealfon R, Tolstorukov MY, Will S, Alekseyenko AA, Artieri C, Booth BW, Brooks AN, Dai Q, Davis CA, Duff MO, Feng X, Gorchakov AA, Gu T, Henikoff JG, Kapranov P, Li R, MacAlpine HK, Malone J, Minoda A, Nordman J, Okamura K, Perry M, Powell SK, Riddle NC, Sakai A, Samsonova A, Sandler JE, Schwartz YB, Sher N, Spokony R, Sturgill D, van Baren M, Wan KH, Yang L, Yu C, Feingold E, Good P, Guyer M, Lowdon R, Ahmad K, Andrews J, Berger B, Brenner SE, Brent MR, Cherbas L, Elgin SC, Gingeras TR, Grossman R, Hoskins RA, Kaufman TC, Kent W, Kuroda MI, Orr-Weaver T, Perrimon N, Pirrotta V, Posakony JW, Ren B, Russell S, Cherbas P, Graveley BR, Lewis S, Micklem G, Oliver B, Park PJ, Celniker SE, Henikoff S, Karpen GH, Lai EC, MacAlpine DM, Stein LD, White KP, Kellis M.",
-                    "short_authors": "modENCODE Consortium",
+                    "short_authors": "modENCODE Consortium et al.",
                     "date": "24 December 2010",
                     "vol": "330 no. 6012",
                     "pp":"1787-1797",
@@ -83,7 +83,7 @@ publications = {
                 {
                     "title": "Plasticity in patterns of histone modifications and chromosomal proteins in Drosophila heterochromatin.",
                     "full_authors": "Riddle NC, Minoda A, Kharchenko PV, Alekseyenko AA, Schwartz YB, Tolstorukov MY, Gorchakov AA, Jaffe JD, Kennedy C, Linder-Basso D, Peach SE, Shanower G, Zheng H, Kuroda MI, Pirrotta V, Park PJ, Elgin SC, Karpen GH.",
-                    "short_authors": "Riddle NC",
+                    "short_authors": "Riddle NC et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"147-163",
@@ -93,7 +93,7 @@ publications = {
                 {
                     "title": "Chromatin signatures of the Drosophila replication program.",
                     "full_authors": "Eaton ML, Prinz JA, MacAlpine HK, Tretyakov G, Kharchenko PV, MacAlpine DM.",
-                    "short_authors": "Eaton ML",
+                    "short_authors": "Eaton ML et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"164-174",
@@ -103,7 +103,7 @@ publications = {
                 {
                     "title": "Developmental control of the DNA replication and transcription programs.",
                     "full_authors": "Nordman J, Li S, Eng T, Macalpine D, Orr-Weaver TL.",
-                    "short_authors": "Nordman J",
+                    "short_authors": "Nordman J et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"175-181",
@@ -113,7 +113,7 @@ publications = {
                 {
                     "title": "Genome-wide analysis of promoter architecture in Drosophila melanogaster.",
                     "full_authors": "Hoskins RA, Landolin JM, Brown JB, Sandler JE, Takahashi H, Lassmann T, Yu C, Booth BW, Zhang D, Wan KH, Yang L, Boley N, Andrews J, Kaufman TC, Graveley BR, Bickel PJ, Carninci P, Carlson JW, Celniker SE.",
-                    "short_authors": "Hoskins RA",
+                    "short_authors": "Hoskins RA et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"182-192",
@@ -123,7 +123,7 @@ publications = {
                 {
                     "title": "Conservation of an RNA regulatory map between Drosophila and mammals.",
                     "full_authors": "Brooks AN, Yang L, Duff MO, Hansen KD, Park JW, Dudoit S, Brenner SE, Graveley BR.",
-                    "short_authors": "Brooks AN",
+                    "short_authors": "Brooks AN et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"193-202",
@@ -133,7 +133,7 @@ publications = {
                 {
                     "title": "Deep annotation of Drosophila melanogaster microRNAs yields insights into their processing, modification, and emergence.",
                     "full_authors": "Berezikov E, Robine N, Samsonova A, Westholm JO, Naqvi A, Hung JH, Okamura K, Dai Q, Bortolamiol-Becet D, Martin R, Zhao Y, Zamore PD, Hannon GJ, Marra MA, Weng Z, Perrimon N, Lai EC.",
-                    "short_authors": "Berezikov E",
+                    "short_authors": "Berezikov E et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"203-215",
@@ -143,7 +143,7 @@ publications = {
                 {
                     "title": "Polycomb preferentially targets stalled promoters of coding and noncoding transcripts.",
                     "full_authors": "Enderle D, Beisel C, Stadler MB, Gerstung M, Athri P, Paro R.",
-                    "short_authors": "Enderle D",
+                    "short_authors": "Enderle D et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"216-226",
@@ -153,7 +153,7 @@ publications = {
                 {
                     "title": "Broad chromosomal domains of histone modification patterns in C. elegans.",
                     "full_authors": "Liu T, Rechtsteiner A, Egelhofer TA, Vielle A, Latorre I, Cheung MS, Ercan S, Ikegami K, Jensen M, Kolasinska-Zwierz P, Rosenbaum H, Shin H, Taing S, Takasaki T, Iniguez AL, Desai A, Dernburg AF, Kimura H, Lieb JD, Ahringer J, Strome S, Liu XS.",
-                    "short_authors": "Liu T",
+                    "short_authors": "Liu T et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"227-236",
@@ -163,7 +163,7 @@ publications = {
                 {
                     "title": "High nucleosome occupancy is encoded at X-linked gene promoters in C. elegans.",
                     "full_authors": "Ercan S, Lubling Y, Segal E, Lieb JD.",
-                    "short_authors": "Ercan S",
+                    "short_authors": "Ercan S et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"237-244",
@@ -173,7 +173,7 @@ publications = {
                 {
                     "title": "Diverse transcription factor binding features revealed by genome-wide ChIP-seq in C. elegans.",
                     "full_authors": "Niu W, Lu ZJ, Zhong M, Sarov M, Murray JI, Brdlik CM, Janette J, Chen C, Alves P, Preston E, Slightham C, Jiang L, Hyman AA, Kim SK, Waterston RH, Gerstein M, Snyder M, Reinke V.",
-                    "short_authors": "Niu W",
+                    "short_authors": "Niu W et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"245-254",
@@ -183,7 +183,7 @@ publications = {
                 {
                     "title": "A global analysis of C. elegans trans-splicing.",
                     "full_authors": "Allen MA, Hillier LW, Waterston RH, Blumenthal T.",
-                    "short_authors": "Allen MA",
+                    "short_authors": "Allen MA et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"255-264",
@@ -193,7 +193,7 @@ publications = {
                 {
                     "title": "Multimodal RNA-seq using single-strand, double-strand, and CircLigase-based capture yields a refined and extended description of the C. elegans transcriptome.",
                     "full_authors": "Lamm AT, Stadler MR, Zhang H, Gent JI, Fire AZ.",
-                    "short_authors": "Lamm AT",
+                    "short_authors": "Lamm AT et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"265-275",
@@ -203,7 +203,7 @@ publications = {
                 {
                     "title": "Prediction and characterization of noncoding RNAs in C. elegans by integrating conservation, secondary structure, and high-throughput sequencing and array data.",
                     "full_authors": "Lu ZJ, Yip KY, Wang G, Shou C, Hillier LW, Khurana E, Agarwal A, Auerbach R, Rozowsky J, Cheng C, Kato M, Miller DM, Slack F, Snyder M, Waterston RH, Reinke V, Gerstein MB.",
-                    "short_authors": "Lu ZJ",
+                    "short_authors": "Lu ZJ et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"276-285",
@@ -213,7 +213,7 @@ publications = {
                 {
                     "title": "Computational and experimental identification of mirtrons in Drosophila melanogaster and Caenorhabditis elegans.",
                     "full_authors": "Chung WJ, Agius P, Westholm JO, Chen M, Okamura K, Robine N, Leslie CS, Lai EC.",
-                    "short_authors": "Chung WJ",
+                    "short_authors": "Chung WJ et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"286-300",
@@ -223,7 +223,7 @@ publications = {
                 {
                     "title": "The transcriptional diversity of 25 Drosophila cell lines.",
                     "full_authors": "Cherbas L, Willingham A, Zhang D, Yang L, Zou Y, Eads BD, Carlson JW, Landolin JM, Kapranov P, Dumais J, Samsonova A, Choi JH, Roberts J, Davis CA, Tang H, van Baren MJ, Ghosh S, Dobin A, Bell K, Lin W, Langton L, Duff MO, Tenney AE, Zaleski C, Brent MR, Hoskins RA, Kaufman TC, Andrews J, Graveley BR, Perrimon N, Celniker SE, Gingeras TR, Cherbas P.",
-                    "short_authors": "Cherbas L",
+                    "short_authors": "Cherbas L et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"301-314",
@@ -233,7 +233,7 @@ publications = {
                 {
                     "title": "The Drosophila melanogaster transcriptome by paired-end RNA sequencing.",
                     "full_authors": "Daines B, Wang H, Wang L, Li Y, Han Y, Emmert D, Gelbart W, Wang X, Li W, Gibbs R, Chen R.",
-                    "short_authors": "Daines B",
+                    "short_authors": "Daines B et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"315-324",
@@ -243,7 +243,7 @@ publications = {
                 {
                     "title": "A spatial and temporal map of C. elegans gene expression.",
                     "full_authors": "Spencer WC, Zeller G, Watson JD, Henz SR, Watkins KL, McWhirter RD, Petersen S, Sreedharan VT, Widmer C, Jo J, Reinke V, Petrella L, Strome S, Von Stetina SE, Katz M, Shaham S, Rätsch G, Miller DM 3rd.",
-                    "short_authors": "Spencer WC",
+                    "short_authors": "Spencer WC et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"325-341",
@@ -253,7 +253,7 @@ publications = {
                 {
                     "title": "Genome-wide analysis of alternative splicing in Caenorhabditis elegans.",
                     "full_authors": "Ramani AK, Calarco JA, Pan Q, Mavandadi S, Wang Y, Nelson AC, Lee LJ, Morris Q, Blencowe BJ, Zhen M, Fraser AG.",
-                    "short_authors": "Ramani AK",
+                    "short_authors": "Ramani AK et al.",
                     "date": "February 2011",
                     "vol": "21",
                     "page":"342-348",
