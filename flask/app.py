5f949466bc7a8070d3191bc8420c7f146a583478
Fengyuan Hu <fengyuan.huu@gmail.com>
2011-08-17 16:28:43 +0100

    updated to Radek's latest version

--- flask/app.py
+++ flask/app.py
@@ -8,7 +8,7 @@ from flask import Flask
 import config
 
 # utils
-import re, unicodedata, urllib
+import re, unicodedata, urllib, time
 
 app = None
 
@@ -64,7 +64,49 @@ def create_app():
 
     @app.template_filter('url_encode')
     def url_encode(uri):
-       return urllib.quote_plus(uri)
+        return urllib.quote_plus(uri)
+
+    @app.template_filter('article_image')
+    def article_image(article):
+        """
+        Determine article image name
+        """
+        return slugify('-'.join([article['journal'], article['vol'], article['date']]))
+
+    @app.template_filter('shorten_authors')
+    def shorten_authors(authors, title, limit=140):
+        """
+        Shorten the authors listing if too long
+        """
+        limit -= len(title)
+        result = []
+        for author in authors.split(', '):
+            limit -= len(author)
+            result.append(author)
+            if limit < 0: break
+        return ', '.join(result) + '&hellip;' if limit < 0 else ', '.join(result)
+
+    @app.template_filter('flatten_publications')
+    def flatten_publications(pubs):
+        """
+        Make a dict into a list of individual articles with journal field
+        """
+        articles = []
+        for journal in pubs['journal']:
+            for article in journal['articles']:
+                article['journal'] = journal['name']
+                articles.append(article)
+        return sorted(articles,
+                      key=lambda a: time.mktime(time.strptime(a['date'], "%d %B %Y" if len(a['date'].split()) == 3 else "%B %Y")),
+                      reverse=True)
+
+    @app.template_filter('italics')
+    def italics(text):
+        """
+        Apply italics to genus etc.
+        """
+        return re.sub(re.compile('('+('|'.join(['C\. elegans', 'Caenorhabditis elegans', 'D\. melanogaster', 'Drosophila melanogaster',
+        'Drosophila', 'cis-', 'trans-']))+')'), lambda m: "<i>%s</i>" % m.group(1), text)
 
     return app
 
