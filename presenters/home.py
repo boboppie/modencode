dfc79a2bca110324556caf43c967af39e9a01458
Radek Stepan <radek.stepan@gmail.com>
2011-09-06 18:30:53 +0100

    simple matching of ip to allow ppl to call /update

--- presenters/home.py
+++ presenters/home.py
@@ -4,10 +4,11 @@
 import config
 
 # framework
-from flask import Blueprint, render_template, request, redirect, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for
 
 # imports
 from libs import utils
+from functools import wraps
 
 # models
 from models.modmine import Modmine
@@ -16,16 +17,29 @@ from models.constants import *
 
 home = Blueprint('home', __name__)
 
+# allowed ip addresses to call /update
+allowed_ip = ['127.0.0.1']
+
+def match_ip(f):
+    """
+    match a specific ip address when calling this function
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        remote = request.remote_addr
+        if remote not in allowed_ip:
+            return redirect(url_for('home.index'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @home.route('/')
 def index(update=False):
     """
     serve home page
     """
-    t = Templates('index') # create a new Template object from "static/html/index.html", it doesn't exist the first call
+    t = Templates('index')
 
     if update or not t.exists():
-        # fetch data
         m = Modmine()
         data = m.get_catexp_data()
         gbrowse_base = m.get_gbrowse_base()
@@ -35,11 +49,12 @@ def index(update=False):
         from models.publications import publications as modencode_publications
 
         html = render_template('home/index.html', **dict(locals().items() + globals().items()))
-        t.write(html) # create a new static/html/index.html if not exist
+        t.write(html)
 
     return t.read()
 
 @home.route('/update')
+@match_ip
 def update():
     """
     fetch updates from modmine
