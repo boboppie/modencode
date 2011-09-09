ab2a4f8ae1f27fc7d41c05c91bcec2b0be89e029
Radek Stepan <radek.stepan@gmail.com>
2011-09-09 12:23:29 +0100

    tabs to spaces

--- libs/mail.py
+++ libs/mail.py
@@ -5,27 +5,27 @@ import config
 import smtplib
  
 def send(from_address, message):
- 	# form the message
-	message = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (
-		from_address,
-		','.join(config.MAIL_RECIPIENTS),
-		'modENCODE Contact Form',
-		message)
+    # form the message
+    message = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (
+    from_address,
+    ','.join(config.MAIL_RECIPIENTS),
+    'modENCODE Contact Form',
+    message)
 
     # SMTP server
-	server = smtplib.SMTP(host=config.MAIL_SERVER, port=config.MAIL_PORT, timeout=10)
+    server = smtplib.SMTP(host=config.MAIL_SERVER, port=config.MAIL_PORT, timeout=10)
 
-	# use TLS?
-	if config.MAIL_USE_TLS:
-		server.ehlo()
-		server.starttls()
-		server.ehlo()
-	
-	# login
-	server.login(config.MAIL_USERNAME, config.MAIL_PASSWORD)
+    # use TLS?
+    if config.MAIL_USE_TLS:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
 
-	# send & quit
-	problems = server.sendmail(from_address, config.MAIL_RECIPIENTS, message)
-	server.quit()
+    # login
+    server.login(config.MAIL_USERNAME, config.MAIL_PASSWORD)
 
-	return problems
+    # send & quit
+    problems = server.sendmail(from_address, config.MAIL_RECIPIENTS, message)
+    server.quit()
+
+    return problems
